1fd283eb921cc824b8564a4406fed7fc062d301e
Matias <mngilbarbosa@gmail.com>
2026-04-18 10:47:32 -0300

    cofre antiguo y enemigos con derrota

--- Actividades_Gil/Ejercicio9.py
+++ Actividades_Gil/Ejercicio9.py
@@ -9,6 +9,7 @@ class CofreAbierto():
         self.prob_vacio = 20
         self.monedas = random.randint(self.contenido[0], self.contenido[1])
         self.estado = random.randint(1, 100)
+        self.abierto = True
     
     def Investigar(self):
         if self.estado != "vacio":
@@ -20,16 +21,22 @@ class CofreAbierto():
         print("...")
         if self.estado != "vacio":
             print(f"El cofre contiene {self.monedas} monedas")
+            return("tesoro", self.monedas)
         else:
             print("El cofre esta vacio")
+            return("vacio", 0)
 
     def Recolectar(self):
-        if self.estado != "vacio":
-            print(f"Has recolectado {self.monedas} monedas")
-            return self.monedas
-        else:
-            print("El cofre esta vacio!!")
+        if self.abierto == False:
+            print("el cofre esta cerrado")
             return 0
+        else:
+            if self.estado != "vacio":
+                print(f"Has recolectado {self.monedas} monedas")
+                return self.monedas
+            else:
+                print("El cofre esta vacio!!")
+                return 0
 
     def Patear(self):
         print("...por que? ya esta abierto")
@@ -60,14 +67,28 @@ class CofreCerrado():
     def Abrir(self):
         pass
     
-    @abstractmethod
     def Recolectar(self):
-        pass
+        if self.abierto == False:
+            print("el cofre esta cerrado")
+            return 0
+        else:
+            if self.estado == "vacio":
+                print("el cofre esta vacio")
+                return 0
+            else:
+                print(f"¡has encontrado {self.monedas} monedas!")
+            return self.monedas
     
-    @abstractmethod
     def Patear(self):
-        pass
-
+        if self.estado == "vacio":
+            print("el cofre suena medio hueco")
+        elif self.estado == "malo":
+            print("el cofre te parece pesado")
+        else:
+            if self.monedas > self.contenido_bueno[1] // 2:
+                print("el cofre te parece pesado")
+            else:
+                print("el cofre se mueve con facilidad")
 
 class CofreMadera(CofreCerrado):
     def __init__(self):
@@ -91,25 +112,34 @@ class CofreMadera(CofreCerrado):
             print("el cofre tiene un tesoro!")
             self.abierto = True
             return "tesoro"
-    
-    def Recolectar(self):
+
+class CofreAntiguo(CofreCerrado):
+    def __init__(self):
+        descripcion = "este es un cofre antiguo, capaz es el tesoro de un pirata."
+        super().__init__(20, descripcion)
+        self.contenido_bueno = (10, 20)
+        self.contenido_malo = {"Gnomo ladron": 70,
+                                "Duende Pequeño": 30}
+        self.monedas = random.randint(self.contenido_bueno[0], self.contenido_bueno[1])
+
+    def Abrir(self):
         if self.estado == "vacio":
             print("el cofre esta vacio")
-            return 0
-        else:
-            print(f"¡has encontrado {self.monedas} monedas!")
-            return self.monedas
-    
-    def Patear(self):
-        if self.estado == "vacio":
-            print("el cofre suena medio hueco")
+            self.abierto = True
+            return "vacio"
         elif self.estado == "malo":
-            print("el cofre te parece pesado")
-        else:
-            if self.monedas > self.contenido_bueno[1]:
-                print("el cofre te parece pesado")
+            enemigo = random.choice(list(self.contenido_malo.keys()))
+            print(f"has abierto el cofre... y te asalta un {enemigo}")
+            if enemigo == "Gnomo ladron":
+                return ("ladron", enemigo)
             else:
-                print("el cofre se mueve con facilidad")
+                return ("picaro", enemigo)
+            self.estado = "vacio"
+            self.abierto = True
+        else:
+            print("el cofre tiene un tesoro!")
+            self.abierto = True
+            return "tesoro"
 
 cofres_posibles = {
     "ninguno": {
@@ -122,7 +152,11 @@ cofres_posibles = {
     },
     "cofre_madera": {
         "objeto": CofreMadera,
-        "probabilidad": 50
+        "probabilidad": 30
+    },
+    "cofre_antiguo": {
+        "objeto": CofreAntiguo,
+        "probabilidad": 20
     }
 }
 
@@ -144,7 +178,8 @@ class Juego():
             print("1. Abrir")
             print("2. Recolectar")
             print("3. Patear")
-            print("4. Salir")
+            print("4. Investigar")
+            print("5. Avanzar")
             opcion = input("\nOpcion: ")
             if opcion == "1":
                 resultado = cofre.Abrir()
@@ -159,8 +194,17 @@ class Juego():
                         print(f"el {resultado[1]} se va a regañadientes")
                     else:
                         print("has perdido!")
-                        print(f"el {resultado[1]} se queda con la mitad de tus monedas")
-                        self.tesoros_acumulados -= self.tesoros_acumulados // 2
+                        if self.tesoros_acumulados // 2 > 15:
+                            print(f"el {resultado[1]} se queda con la mitad de tus monedas")
+                            self.tesoros_acumulados -= self.tesoros_acumulados // 2
+                        else:
+                            print(f"el {resultado[1]} se queda con 15 monedas")
+                            self.tesoros_acumulados -= 15
+                        break
+                elif resultado[0] == "picaro":
+                    print(f"el {resultado[1]} te roba y se escabulle antes de que puedas reaccionar")
+                    self.tesoros_acumulados -= self.tesoros_acumulados // 4
+                    break
 
             elif opcion == "2":
                 resultado = cofre.Recolectar()
@@ -169,6 +213,9 @@ class Juego():
             elif opcion == "3":
                 cofre.Patear()
             elif opcion == "4":
+                cofre.Investigar()
+
+            elif opcion == "5":
                 break
 
 
@@ -182,21 +229,26 @@ class Juego():
             self.Mostrar_Estado()
             probabilidad_cofre = random.randint(1, 100)
             probabilidad_acumulada = 0
+            cofre_actual = None
             for cofre in cofres_posibles:
                 probabilidad_acumulada += cofres_posibles[cofre]["probabilidad"]
                 if probabilidad_cofre <= probabilidad_acumulada:
-                    if cofres_posibles[cofre]["objeto"] != None:
-                        cofre_actual = cofres_posibles[cofre]["objeto"]()
-                    else:
-                        cofre_actual = None
+                    if cofre_actual == None:
+                        if cofres_posibles[cofre]["objeto"] != None:
+                            cofre_actual = cofres_posibles[cofre]["objeto"]()
+                        else:
+                            cofre_actual = None
             if cofre_actual != None:
                 print(f"has encontrado un {cofre_actual.nombre}!")
                 self.Interactuar_Cofre(cofre_actual)
             else:
                 print("no has encontrado nada... sigue bajando")
                 input("presiona ENTER para continuar")
+            if self.tesoros_acumulados < 0:
+                print(f"has perdido! te han robado todas tus monedas")
+                print("GAME OVER")
+                break
             self.profundidad += 1
 
 Partida = Juego()
 Partida.Comienzar_Juego()
-    
