4311bbdd1ab4fb867327aacd247ddfb5ca232be6
Matias <mngilbarbosa@gmail.com>
2026-04-22 19:22:07 -0300

    Ejercicio 14: clase Horda, capz de hacer la parte automatica del juego, cambian los dibujos por vida y por recargando, hace daño a la torre, las hordas deben ser configuradas externamente, proximamente por la clase Partida la cual incluira la interaccion del usuario con las decisiones sobre las mejoras y el aumento de la dificultad de la horda de forma prograsiva

--- Actividades_Gil/Ejercicio14.py
+++ Actividades_Gil/Ejercicio14.py
@@ -4,9 +4,9 @@ import time
 def Mostrar_estado_juego(lista_ataque_enemigo, torre):
     tropas_en_camino = list(map(lambda tropa: tropa if tropa == "-"  else tropa.dibujo if tropa.dibujo != "" else "-", lista_ataque_enemigo))
     tropas_visibles = "".join(tropas_en_camino)
-    print(tropas_en_camino) # TEST
+    #print(tropas_en_camino) # TEST
 
-    print("visibles", tropas_visibles) # TEST
+    #print("visibles", tropas_visibles) # TEST
     lineas_torre = []
     cant_arqueros = len(torre.arqueros)
     for i in range(1, 6):
@@ -46,10 +46,11 @@ class Tropa():
     def RecibirGolpe(self, danio):
         self.vida -= danio
         if self.vida:
+            #print("recibio golpe")
             self.dibujo_actual += 1
             self.dibujo = self.lista_dibujos[self.dibujo_actual]
         else:
-            self.dibujo = ""
+            self.dibujo = "-"
 
     def Atacar(self, enemigo):
         if isinstance(enemigo, Tropa):
@@ -60,18 +61,25 @@ class Tropa():
     def Avanzar(self, tropa_proxima):
         # retorna True si la tropa puede avanzar evitando una superposicion de tropas y sea printeable el juego
         if tropa_proxima != "-":
-            if tropa_proxima.dibujo != "":
+            if tropa_proxima.dibujo != "-":
                 if tropa_proxima.bando == self.bando:
                     return False
                 else:
                     self.Atacar(tropa_proxima)
                     return False
             else:
-                print("se puede avanzar")
+                #print("se puede avanzar")
                 return True
         else:
-            print("se puede avanzar")
+            #print("se puede avanzar")
             return True
+
+
+class Barbaro(Tropa):
+    def __init__(self, bando):
+        super().__init__(2, 1, ["o", "x"], 2, bando)
+    
+
 
 class Arquero():
     def __init__(self, distancia):
@@ -94,18 +102,21 @@ class Arquero():
             objetivo = lista_camino[casilla]
             if not enemigo:
                 if objetivo != "-":
-                    if objetivo.bando != self.bando:
-                        enemigo = objetivo
+                    if objetivo.dibujo != "-":
+                        if objetivo.bando != self.bando:
+                            enemigo = objetivo
         
         if enemigo:
             if not self.recargando:
                 Disparar(enemigo)
                 self.recargando = True
                 self.bandera_recarga += 1
+                self.dibujo = self.lista_dibujos[1]
             else:
                 if self.bandera_recarga - self.velocidad <= 0:
                     self.recargando = False
                     self.bandera_recarga = 0
+                    self.dibujo = self.lista_dibujos[0]
 
 class Torre():
     def __init__(self):
@@ -154,12 +165,16 @@ class Torre():
                 self.vida = 100
 
     def RecibirGolpe(self, danio):
-        self.vida -= danio - self.defenza
+        self.vida -= danio - self.defensa
+
+    def Arqueros_Atacar(self, lista_camino):
+        for arquero in self.arqueros:
+            arquero.Defender(lista_camino)
 
 
-tropa1 = Tropa(0, 2, ["o", "x"], 1, 0)
-tropa2 = Tropa(0, 2, ["o", "x"], 1, 0)
-tropa3 = Tropa(0, 2, ["o", "x"], 1, 0)
+tropa1 = Barbaro(2)
+tropa2 = Barbaro(2)
+tropa3 = Barbaro(2)
 
 lista_camino = []
 for i in range(1, 21):
@@ -167,36 +182,76 @@ for i in range(1, 21):
 
 lista = [tropa1, tropa2, tropa3]
 
-def Avanzar(lista_camino, lista, torre):
-    for i in range(len(lista_camino) - 1, -1, -1):
-        #print(f"casilla {i}")
-        casilla_actual = lista_camino[i]
-        if i == 0:
-            if lista:
-                casilla_anterior = lista[-1]
+class Horda():
+    def __init__(self, cantidad, lista_camino, torre, remuneracion):
+        self.lista_tropas = []
+        for i in range(cantidad):
+            self.lista_tropas.append(Barbaro(2))
+        self.torre = torre
+        self.lista_camino = lista_camino
+        self.remuneracion = remuneracion
+
+    def Atacar(self):
+        if not isinstance(self.lista_camino[-1], str):
+            self.lista_camino[-1].Atacar(self.torre)
+
+    def Avanzar(self):
+        print("-"*30)
+        self.torre.Arqueros_Atacar(self.lista_camino)
+        self.Atacar()
+        for i in range(len(self.lista_camino) - 1, -1, -1):
+            #print(f"casilla {i}")
+            casilla_actual = self.lista_camino[i]
+            if i == 0:
+                if self.lista_tropas:
+                    casilla_anterior = self.lista_tropas[-1]
+                    if not isinstance(casilla_anterior, str):
+                        if casilla_anterior.Avanzar(casilla_actual):
+                            self.lista_camino[i] = casilla_anterior
+                            self.lista_tropas.pop(-1)
+                    else:
+                        casilla_actual = casilla_anterior
+                        casilla_anterior = "-"
+            else:
+                casilla_anterior = self.lista_camino[i - 1]
                 if not isinstance(casilla_anterior, str):
                     if casilla_anterior.Avanzar(casilla_actual):
-                        lista_camino[i] = casilla_anterior
-                        lista.pop(-1)
+                        self.lista_camino[i] = casilla_anterior
+                        self.lista_camino[i - 1] = "-"
                 else:
                     casilla_actual = casilla_anterior
                     casilla_anterior = "-"
-        else:
-            casilla_anterior = lista_camino[i - 1]
-            if not isinstance(casilla_anterior, str):
-                if casilla_anterior.Avanzar(casilla_actual):
-                    lista_camino[i] = casilla_anterior
-                    lista_camino[i - 1] = "-"
-            else:
-                casilla_actual = casilla_anterior
-                casilla_anterior = "-"
-        #print(f"{casilla_actual} | {casilla_anterior}")
-        time.sleep(0.1)
-    print(lista)
-    Mostrar_estado_juego(lista_camino, torre)
-    time.sleep(0.7)
+            #print(f"{casilla_actual} | {casilla_anterior}")
+            #time.sleep(0.1)
+        #print(self.lista_tropas)
+        
+        for tropa in self.lista_tropas:
+            if tropa.vida <= 0:
+                self.lista_tropas.remove(tropa)
+                self.lista_camino[i] = "-"
+        #print(self.lista_tropas)
+        Mostrar_estado_juego(self.lista_camino, self.torre)
+        print("-"*30)
+        time.sleep(0.7)
 
+    def Iniciar_Horda(self):
+        while True:
+            self.Avanzar()
+            finalizo = True
+            for tropa in self.lista_camino:
+                if not isinstance(tropa, str):
+                    if tropa.vida > 0:
+                        finalizo = False
+            if finalizo: 
+                break
+        print("sobreviviste a la ronda!")
+        self.torre.monedas += self.remuneracion
+        print(f"recibiste {self.remuneracion} monedas")
+        print(f"tienes {self.torre.vida} puntos de vida")
+        print(f"tienes {self.torre.monedas} monedas")
+    
 torre = Torre()
-for i in range(1, 21):
-    Avanzar(lista_camino, lista, torre)
+remuneracion = 5
+horda = Horda(4, lista_camino, torre, remuneracion)
+horda.Iniciar_Horda()
     
