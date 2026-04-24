211a2e957384c14a9ba0a9db03203bf82d08c288
Matias <mngilbarbosa@gmail.com>
2026-04-23 22:34:05 -0300

    Ejercicio 12: ya es posible investigar el agua y el suelo de la balsa de la clase ambiente, al elegir buscar un objeto del agua van haciendo pasos y timelapses ademas de poder ser atacado por el tiburon. Mas clases de utilidad y funcionalidad del Barril

---
+++ Actividades_Gil/Ejercicio12.py
@@ -0,0 +1,287 @@
+from Ejercicio12_objects import Recursos_no_Usables, Recursos_Usables, Recursos_Unicos
+import random
+
+class Inventario():
+
+    def __init__(self, jugador, cant_maxima):
+        self.cantidad_maxima = cant_maxima
+        self.inventario = []
+        self.jugador = jugador
+    
+    def Guardar(self, objeto):
+        nuevo = True
+        for objeto_espacio in self.inventario:
+            if objeto_espacio.nombre == objeto.nombre:
+                objeto_espacio.cantidad += objeto.cantidad
+                nuevo = False
+        
+        if nuevo:
+            if len(self.inventario) < self.cantidad_maxima:
+                self.inventario.append(objeto)
+            else:
+                print("no hay mas espacio")
+
+    def Descartar(self, indice):
+        objeto = self.inventario.pop(indice)
+        return objeto
+
+    def Usar(self, indice):
+        print("usando:", indice)
+        objeto = self.inventario[indice]
+        if objeto.usable:
+            objeto.Usar(self.jugador)
+            if objeto.cantidad <= 0:
+                self.inventario.pop(indice)
+            self.Lista()
+        else:
+            print("este objeto no se puede usar")
+            self.Lista()
+
+    def Inspeccionar(self, objeto, indice):
+        print("nombre:", objeto.nombre)
+        print("cantidad:", objeto.cantidad)
+        print("descripcion:", objeto.descripcion)
+        print("que desea hacer? ")
+        print("1- Usar")
+        print("2- Descartar")
+        print("3 o enter- salir")
+        eleccion = input("-- ")
+        if eleccion != "":
+            eleccion = int(eleccion)
+            if eleccion != 3:
+                if eleccion == 1:
+                    self.Usar(indice)
+                elif eleccion == 2:
+                    objeto = self.Descartar(indice)
+                    self.Lista()
+                else:
+                    self.Lista()
+            else:
+                self.Lista()
+        else:
+            self.Lista()
+
+
+    def Lista(self):
+        for i in range(len(self.inventario)):
+            print(f"espacio {i + 1}- {self.inventario[i].nombre}")
+        print("enter para salir o ingrese el numero de espacio para inspeccionar")
+        eleccion = input()
+        if eleccion != "":
+            indice_eleccion = int(eleccion) - 1
+            objeto = self.inventario[indice_eleccion]
+            self.Inspeccionar(objeto, indice_eleccion)
+        else:
+            return
+
+
+class Jugador():
+    """
+    CLASE DE DECISIONES; sera la parte interactiva del juego con el entorno y con si mismo. Avisara a la clase partida cuando deba morir.
+    y que decide hacer con los objetos que encuentra.
+    """
+    def __init__(self, ambiente):
+        self.vida = 100
+        self.sed = 100
+        self.hambre = 100
+        self.inventario = Inventario(self, 10)
+        self.ambiente = ambiente # referencia al entorno en el que se encuentra el jugador
+    
+    def Aumentar_inventario(self):
+        self.inventario.cantidad_maxima += 1
+        print(f"ahora tienes {self.inventario.cantidad_maxima} espacios en el inventario")
+    
+    def Aumentar_vida(self, vida_aumentar):
+        self.vida += vida_aumentar
+        if self.vida > 100:
+            self.vida = 100
+    
+    def Aumentar_sed(self, sed_aumentar):
+        self.sed += sed_aumentar
+        if self.sed > 100:
+            self.sed = 100
+    
+    def Aumentar_hambre(self, hambre_aumentar):
+        self.hambre += hambre_aumentar
+        if self.hambre > 100:
+            self.hambre = 100
+    
+    def Mostrar_estado(self):
+        print("vida:", self.vida)
+        print("sed:", self.sed)
+        print("hambre:", self.hambre)
+    
+    def Mostrar_inventario(self):
+        self.inventario.Lista()
+    
+    def Timelapse(self):
+        # es el precio de hacer cualquier accion, es para simular el paso del tiempo
+        # debido a que no hay un bucle principal que controle el tiempo y el jugador puede hacer lo que quiera 
+        # ademas el programa cada vez que actualice el entorno se ejecutara este metodo
+        self.sed -= 1
+        self.hambre -= 1
+        if self.sed <= 0 or self.hambre <= 0:
+            self.vida = 0
+        elif self.sed <= 30 or self.hambre <= 30:
+            self.vida -= 10
+        elif self.sed <= 60 or self.hambre <= 60:
+            self.vida -= 5
+        self.ambiente.Actualizar()
+    def Inspeccionar_Objeto(self, objeto):
+        print("nombre: ", objeto.nombre)
+        print("cantidad: ", objeto.cantidad)
+        print("descripcion: ", objeto.descripcion)
+        if isinstance(objeto, type(Recursos_Unicos.Barril())):
+            print("enter para abrir el barril")
+            input()
+            objeto.Abrir(self)
+            return False
+        else:
+            print("lo quieres guardar? (si/no)")
+            eleccion = input()
+            if eleccion == "si":
+                self.inventario.Guardar(objeto)
+                return True # esto sera para avisar si hay que sacarlo del suelo
+            else:
+                return False
+
+    def Tiburon(self):
+        if random.randint(1, 100) <= self.ambiente.prob_tiburon:
+            print("un tiburon te ataca")
+            self.vida -= 10
+            self.ambiente.prob_tiburon = 0
+        else:
+            self.ambiente.prob_tiburon += self.ambiente.prob_tiburon * 1.5
+            if self.ambiente.prob_tiburon > 50:
+                self.ambiente.prob_tiburon = 50
+
+    def Buscar_objeto_agua(self, distancia, objeto):
+        paso = 1
+        while paso <= distancia:
+            self.Timelapse()
+            self.Tiburon()
+            print("paso", paso, "de", distancia)
+            eleccion = input("quieres seguir? (si/no)")
+            if eleccion == "no":
+                print("vuelves a la balsa")
+                return
+            paso += 1
+        if self.Inspeccionar_Objeto(objeto):
+            self.ambiente.Remover_objeto_agua(objeto)
+        print("vuelves a la balsa")
+
+    def Inspeccionar_Agua(self, lista_objetos_agua):
+        for objeto in lista_objetos_agua:
+            if objeto["distancia"] <= 5: 
+                print("logras reconocer un objeto cercano, es un/a:", objeto["objeto"].nombre)
+                if self.ambiente.caña_pescar:
+                    print("con la caña puedes alcanzarlo!")
+                    print("lo quieres alcanzar? (si/no)")
+                    eleccion = input()
+                    if eleccion == "si":
+                        self.inventario.Guardar(objeto["objeto"])
+                        return True # esto sera para avisar si hay que sacarlo del suelo
+                    else:
+                        return False # no se saca del suelo
+                else:
+                    print("no puedes alcanzarlo")
+                    print("quieres meterte al agua a buscarlo? (si/no)")
+                    eleccion = input()
+                    if eleccion == "si":
+                        self.Buscar_objeto_agua(objeto["distancia"], objeto["objeto"])
+            else:
+                print("no logras reconocer bien el objeto")
+                if objeto["distancia"] <= 10:
+                    print("puedes acercarte a inspeccionarlo mejor")
+                    print("quieres acercarte? (si/no)")
+                    eleccion = input()
+                    if eleccion == "si":
+                        self.Buscar_objeto_agua(objeto["distancia"], objeto["objeto"])
+            
+            print("quieres seguir viendo el agua? (si/no)")
+            eleccion = input()
+            if eleccion == "no":
+                return
+
+    def Inspeccionar_Balsa(self, lista_objetos_balsa):
+        for objeto in lista_objetos_balsa:
+            print("en la balsa hay un/a:", objeto["nombre"])
+            print("quieres inspeccionarlo? (si/no)")
+            eleccion = input()
+            if eleccion == "si":
+                if self.Inspeccionar_Objeto(objeto):
+                    self.ambiente.Remover_objeto_balsa(objeto)
+            print("quieres seguir viendo la balsa? (si/no)")
+            eleccion = input()
+            if eleccion == "no":
+                return
+
+    def Decisiones(self):
+        lista_objetos_agua = self.ambiente.Buscar_agua() # son todos los objetos flotantes, tanto barriles como botellas y otros
+        lista_objetos_balsa = self.ambiente.Buscar_balsa() # son todos los objetos en la balsa, tanto palos como piedras y otros
+
+        print("que quieres hacer? ")
+        print("1- Inspeccionar el agua")
+        print("2- Inspeccionar la balsa")
+        print("3- Inspeccionar el inventario")
+        print("4- Salir")
+        eleccion = input("-- ")
+        if eleccion == "1":
+            self.Inspeccionar_Agua(lista_objetos_agua)
+        elif eleccion == "2":
+            self.Inspeccionar_Balsa(lista_objetos_balsa)
+        elif eleccion == "3":
+            self.Mostrar_inventario()
+        elif eleccion == "4":
+            return
+        else:
+            print("opcion no valida")
+            self.Decisiones()
+
+
+class Ambiente():
+    def __init__(self):
+        self.objetos_agua = []
+        self.objetos_balsa = []
+        self.prob_tiburon = 10
+        self.caña_pescar = False
+    
+    def Actualizar(self):
+        cant_nuevos_objetos = random.randint(1, 5)
+        for i in range(cant_nuevos_objetos):
+            probabilidad = random.randint(1, 100)
+            if probabilidad <= 30:
+                objeto_nuevo = Recursos_Unicos.Barril()
+            elif probabilidad <= 65:
+                objeto_nuevo = Recursos_no_Usables.Palo()
+            elif probabilidad <= 100:
+                objeto_nuevo = Recursos_no_Usables.Piedra()
+            self.objetos_agua.append({ "objeto": objeto_nuevo, "distancia": random.randint(1, 10)})
+        
+    def Buscar_agua(self):
+        return self.objetos_agua
+    
+    def Buscar_balsa(self):
+        return self.objetos_balsa
+    
+    def Remover_objeto_agua(self, objeto):
+        self.objetos_agua.remove(objeto)
+    
+    def Remover_objeto_balsa(self, objeto):
+        self.objetos_balsa.remove(objeto)
+
+class Partida():
+    def __init__(self):
+        self.ambiente = Ambiente()
+        self.jugador = Jugador(self.ambiente)
+    
+    def Jugar(self):
+        while True:
+            self.jugador.Timelapse()
+            self.jugador.Decisiones()
+            if self.jugador.vida <= 0:
+                print("game over")
+                break
+
+partida = Partida()
+partida.Jugar()
--- Actividades_Gil/Ejercicio12_objects.py
+++ Actividades_Gil/Ejercicio12_objects.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
 import random 
+
+# CLASE PADRE DE TODOS LOS OBJETOS
+
 class Objeto():
     
     def __init__(self, nombre, usable, descripcion, cantidad, cantidad_maxima):
@@ -24,13 +27,13 @@ class Objeto():
 
 # RECURSOS SIN UTILIDAD DIRECTA
 
-class clase_Palo(Objeto):
+class C_Palo(Objeto):
 
     def __init__(self):
         descripcion = "de madera, capaz de usarse en distintas situaciones"
         super().__init__("Palo", False, descripcion, random.randint(1, 3), 10)
 
-class clase_Piedra(Objeto):
+class C_Piedra(Objeto):
 
     def __init__(self):
         descripcion = "son utiles en distintos objetos, son bastante resistentes y pueden afilarse"
@@ -43,10 +46,10 @@ class clase_Piedra(Objeto):
 
 class Recursos_no_Usables():
     def Palo():
-        return clase_Palo()
+        return C_Palo()
     
     def Piedra():
-        return clase_Piedra()
+        return C_Piedra()
 
 # RECURSOS QUE TIENEN UTILIDAD DIRECTA 
 
@@ -63,6 +66,93 @@ class C_Botella_agua(Objeto):
         jugador.sed += 20
         self.cantidad -= 1
 
+class C_Barra_Cereal(Objeto):
+    def __init__(self):
+        descripcion = "deliciosa, sacia bastante el hambre"
+        nombre = "Barra de Cereal"
+        cantidad = random.randint(1,3)
+        cant_max = 5
+        super().__init__(nombre, True, descripcion, cantidad, cant_max)
+
+    def Utilidad(self, jugador):
+        jugador.hambre += 20
+        self.cantidad -= 1
+
+class C_Kit_Primeros_Auxilios(Objeto):
+    def __init__(self):
+        descripcion = "curativo, cura heridas leves"
+        nombre = "Kit de Primeros Auxilios"
+        cantidad = random.randint(1,3)
+        cant_max = 5
+        super().__init__(nombre, True, descripcion, cantidad, cant_max)
+
+    def Utilidad(self, jugador):
+        jugador.vida += 20
+        self.cantidad -= 1
+
+
 class Recursos_Usables():
     def Botella_agua():
         return C_Botella_agua()
+    def Barra_Cereal():
+        return C_Barra_Cereal()
+    def Kit_Primeros_Auxilios():
+        return C_Kit_Primeros_Auxilios()
+
+# Objetos que requieren combinacion de recursos o son herramientas
+
+class C_Caña_Pescar(Objeto):
+    def __init__(self):
+        descripcion = "permite pescar"  
+        nombre = "Caña de Pescar"
+        cantidad = 1
+        cant_max = 1
+        super().__init__(nombre, True, descripcion, cantidad, cant_max)
+
+    def Utilidad(self, jugador):
+        jugador.pescar()
+        self.cantidad -= 1
+
+
+
+class C_Barril(Objeto):
+    def __init__(self):
+        descripcion = "un barril de madera, puede contener objetos dentro"
+        nombre = "Barril"
+        cantidad = 1
+        cant_max = 1
+        super().__init__(nombre, True, descripcion, cantidad, cant_max)
+        self.objetos = []
+        probabilidad = random.randint(1, 100)
+        if probabilidad <= 15:
+            self.objetos.append(C_Caña_Pescar())
+        elif probabilidad <= 30:
+            self.objetos.append(Recursos_Usables.Barra_Cereal())
+        elif probabilidad <= 45:
+            self.objetos.append(Recursos_Usables.Botella_agua())
+        elif probabilidad <= 60:
+            self.objetos.append(Recursos_Usables.Kit_Primeros_Auxilios())
+        else:
+            probabilidad = random.randint(1, 100)
+            if probabilidad <= 50:
+                self.objetos.append(Recursos_no_Usables.Palo())
+            else:
+                self.objetos.append(Recursos_no_Usables.Piedra())
+    
+    def Abrir(self, jugador):
+        for objeto in self.objetos:
+            jugador.Inspeccionar_Objeto(objeto)
+            if objeto.cantidad <= 0:
+                if objeto.nombre == "Caña de Pescar":
+                    jugador.ambiente.caña_pescar = True
+                self.objetos.remove(objeto)
+        print("has terminado de revisar el barril")
+        self.cantidad -= 1
+
+class Recursos_Unicos():
+
+    def Barril():
+        return C_Barril()
+    
+    def Caña_Pescar():
+        return C_Caña_Pescar()
